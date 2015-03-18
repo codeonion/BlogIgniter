208b4ba417b19390847c7e7d4f57a90e2195de47
Mian Muhammad Imran Shah <shahmian@gmail.com>
2015-03-18 23:40:02 +0500

    Installation file Created.. Signed-off-by: Mian Muhammad Imran Shah <shahmian@gmail.com>

---
+++ application/controllers/blog_setup.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Blog_Setup extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
